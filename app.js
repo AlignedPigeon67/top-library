be0b16e17b80d91d39d96de99773c958f1494434
tomhine <tomhine792@user.github.com>
2020-12-16 20:46:39 +0000

    added delete button and functionality to cards

--- app.js
+++ app.js
@@ -24,8 +24,9 @@ function addBookToLibrary(book) {
 }
 
 function renderLibraryCards() {
-    library.forEach(book => {
+    library.forEach((book, index) => {
         let card = document.createElement('div');
+        let cardDeleteButton = document.createElement('div');
         let titleAuthorSpan = document.createElement('span');
         let cardPages = document.createElement('h2');
         let cardRead = document.createElement('h2');
@@ -33,19 +34,23 @@ function renderLibraryCards() {
         let cardAuthor = document.createElement('h3');
 
         card.classList.add('card');
+        cardDeleteButton.classList.add('card-button', 'card-delete');
         titleAuthorSpan.classList.add('card-text', 'title-author-span');
         cardPages.classList.add('card-text', 'card-pages');
         cardRead.classList.add('card-text', 'card-read');
         cardTitle.classList.add('card-title');
         cardAuthor.classList.add('card-author');
 
+        cardDeleteButton.setAttribute('data-id', index);
+
+        cardDeleteButton.innerHTML = '+';
         cardTitle.innerHTML = book.title;
         cardAuthor.innerHTML = book.author;
         cardPages.innerHTML = `${book.pages} Pages`;
         cardRead.innerHTML = `${book.read ? 'Read' : 'Not Read'}`;
 
         mainContainer.append(card);
-        card.append(titleAuthorSpan, cardPages, cardRead);
+        card.append(cardDeleteButton, titleAuthorSpan, cardPages, cardRead);
         titleAuthorSpan.append(cardTitle, cardAuthor);
     });
 }
@@ -85,8 +90,8 @@ function submitAddBookForm() {
     addBookToLibrary(newBook);
 }
 
-function clearInputText() {
-    let inputs = addBookBox.querySelectorAll('.add-book-text-input');
+function clearInputText(parentElement, classSelector) {
+    let inputs = parentElement.querySelectorAll(classSelector);
 
     inputs.forEach(e => {
         if (e.type === 'text') {
@@ -95,12 +100,16 @@ function clearInputText() {
     });
 }
 
-function clearInputRadio() {
-    let inputs = addBookBox.querySelectorAll('.add-book-radio-input');
+function clearInputRadio(parentElement, classSelector) {
+    let inputs = parentElement.querySelectorAll(classSelector);
 
     inputs.forEach(e => {
         if (e.checked) e.checked = false;
     });
+}
+
+function deleteLibraryItem(dataID) {
+    library.splice(dataID, 1);
 }
 
 let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true);
@@ -114,17 +123,26 @@ addBookToLibrary(horns);
 
 addButton.addEventListener('click', openAddBookForm);
 cancelBookButton.addEventListener('click', () => {
-    clearInputText(addBookBox);
-    clearInputRadio(addBookBox);
+    clearInputText(addBookBox, '.add-book-text-input');
+    clearInputRadio(addBookBox, '.add-book-radio-input');
     closeAddBookForm();
 });
+
 addBookButton.addEventListener('click', () => {
     submitAddBookForm();
-    clearInputText(addBookBox);
-    clearInputRadio(addBookBox);
+    clearInputText(addBookBox, '.add-book-text-input');
+    clearInputRadio(addBookBox, '.add-book-radio-input');
     clearLibraryCards();
     renderLibraryCards();
     closeAddBookForm();
+});
+
+document.addEventListener('click', e => {
+    if (e.target.getAttribute('class') === 'card-button card-delete') {
+        deleteLibraryItem(e.target.getAttribute('data-id'));
+        clearLibraryCards();
+        renderLibraryCards();   
+    }
 });
 
 if (library.length === 0) {
